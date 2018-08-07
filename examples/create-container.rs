24803d3444c7c751039a9f0f8af7b38755493341
eldesh <nephits@gmail.com>
2018-08-07 03:59:46 -0700

    linebreak: dos -> unix

--- examples/create-container.rs
+++ examples/create-container.rs
@@ -1,9 +1,10 @@
 extern crate boondock;
 
-use boondock::{Docker};
+use boondock::{Docker, ContainerCreateOptions};
 
 fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
-    let container = docker.create_container("testing", "openjdk:8").unwrap();
+    let create = ContainerCreateOptions::new("hello-world:linux");
+    let container = docker.create_container("testing", &create).unwrap();
     println!("{:?}", container)
 }
