d8d5a822521004860edbd4ddc79a0e9c038aebb2
eldesh <nephits@gmail.com>
2018-09-04 21:58:24 +0900

    WIP: add auth examples

---
+++ examples/login.rs
@@ -0,0 +1,17 @@
+extern crate dockworker;
+extern crate hyper;
+
+use dockworker::Docker;
+
+fn main() {
+    let docker = Docker::connect_with_defaults().unwrap();
+    let token = docker
+        .auth(
+            "someusername",
+            "somepassword",
+            "someusername@example.com",
+            "localhost:5000",
+        )
+        .unwrap();
+    println!("token: {:?}", token);
+}
