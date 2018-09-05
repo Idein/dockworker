e41b7bc70fcd6444a19e9c835a18d39e29eaeaee
eldesh <nephits@gmail.com>
2018-09-04 19:56:58 -0700

    fix: login example

--- examples/push.rs
+++ examples/push.rs
@@ -1,26 +1,28 @@
 extern crate dockworker;
 extern crate hyper;
 
-use dockworker::Docker;
+use dockworker::{Docker, credentials::{Credential, UserPassword}};
 
 fn main() {
-    let docker = Docker::connect_with_defaults().unwrap();
+    let mut docker = Docker::connect_with_defaults().unwrap();
+
     let (name, tag) = ("alpine", "latest");
     docker
         .create_image(name, tag)
         .unwrap()
         .for_each(|_| print!("."));
+
     let serveraddress = "localhost:5000";
+    docker.set_credential(Credential::with_password(UserPassword::new(
+        "someusername".to_owned(),
+        "somepassword".to_owned(),
+        "someusername@example.com".to_owned(),
+        serveraddress.to_owned(),
+    )));
+
     println!("pulled: {}:{}", name, tag);
     docker
-        .push_image(
-            &format!("{}/{}", serveraddress, name),
-            tag,
-            "someusername",
-            "somepassword",
-            "someusername@example.com",
-            serveraddress,
-        )
+        .push_image(&format!("{}/{}", serveraddress, name), tag)
         .unwrap();
     println!("pushed: {}:{}", name, tag);
 }
