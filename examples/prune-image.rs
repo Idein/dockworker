01db78dea70b6876cb07a2177620993221aafbc6
eldesh <nephits@gmail.com>
2018-09-06 03:29:24 -0700

    example: prune image

---
+++ examples/prune-image.rs
@@ -0,0 +1,21 @@
+extern crate dockworker;
+
+use dockworker::{Docker, container::ContainerFilters};
+
+fn main() {
+    let docker = Docker::connect_with_defaults().unwrap();
+
+    let prunedt = docker.prune_image(true).unwrap();
+    println!("pruned(true): {:?}", prunedt);
+
+    let prunedf = docker.prune_image(false).unwrap();
+    println!("pruned(false): {:?}", prunedf);
+
+    let containers = docker
+        .list_containers(Some(true), None, None, ContainerFilters::new())
+        .unwrap();
+
+    containers.iter().for_each(|c| {
+        println!("image: {}", c.Image);
+    });
+}
