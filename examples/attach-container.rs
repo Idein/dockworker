1419023f3361c6a86ef55838a753007de409f4b1
eldesh <nephits@gmail.com>
2018-09-10 04:05:10 -0700

    WIP: read response of attach api per line

--- examples/attach-container.rs
+++ examples/attach-container.rs
@@ -1,8 +1,10 @@
 extern crate dockworker;
 extern crate hyper;
 
-use dockworker::{ContainerCreateOptions, ContainerHostConfig, Docker};
+use dockworker::{ContainerCreateOptions, ContainerHostConfig, Docker,
+                 container::AttachResponseStreamReader};
 use std::str;
+use std::io::{BufRead, BufReader, Read};
 
 fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
@@ -16,11 +18,41 @@ fn main() {
     let stream = docker
         .attach_container(&container.id, None, true, true, false, true, false)
         .unwrap();
+    let reader: AttachResponseStreamReader = stream.into();
+    let mut line_reader = BufReader::new(reader);
 
-    for frame in stream {
+    loop {
+        let mut line = String::new();
+        let size = line_reader.read_line(&mut line).unwrap();
+        print!("{}: {}", size, line);
+        if size == 0 {
+            break;
+        }
+    }
+    print!("");
+
+    /* // accumurate whole output
+    let mut whole = Vec::new();
+    println!("start");
+    loop {
+        let mut buf: [u8; 7] = [0u8; 7];
+        let x = reader.read(&mut buf).unwrap();
+        println!("read: {}", x);
+        if x == 0 {
+            println!("end of stream");
+            break;
+        }
+        println!("copy");
+        whole.extend_from_slice(&buf);
+    }
+    println!("all: {}", String::from_utf8_lossy(&whole));
+    */
+    /*
+    for frame in iter {
         match frame {
             Ok(frame) => print!("frame:{}", str::from_utf8(frame.as_bytes()).unwrap()),
             Err(err) => println!("frame:err:{:?}", err),
         }
     }
+    */
 }
