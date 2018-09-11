c044e5c358f27eb3f804e9adb69a1136f90d5326
eldesh <nephits@gmail.com>
2018-09-10 23:08:11 -0700

    update attach-container example

--- examples/attach-container.rs
+++ examples/attach-container.rs
@@ -2,7 +2,7 @@ extern crate dockworker;
 extern crate hyper;
 
 use dockworker::{ContainerCreateOptions, ContainerHostConfig, Docker,
-                 container::AttachResponseStreamReader};
+                 container::{self, AttachContainer}};
 use std::str;
 use std::io::{BufRead, BufReader, Read};
 
@@ -15,44 +15,22 @@ fn main() {
 
     let container = docker.create_container(Some("testing"), &create).unwrap();
     docker.start_container(&container.id).unwrap();
-    let stream = docker
+    let mut res = docker
         .attach_container(&container.id, None, true, true, false, true, false)
         .unwrap();
-    let reader: AttachResponseStreamReader = stream.into();
-    let mut line_reader = BufReader::new(reader);
+    res.stdin(container::stdio::Stdio::piped())
+        .stdout(container::stdio::Stdio::piped())
+        .stderr(container::stdio::Stdio::piped());
+    let mut cont: AttachContainer = res.into();
+    let mut line_reader = BufReader::new(cont.stdout);
 
     loop {
         let mut line = String::new();
         let size = line_reader.read_line(&mut line).unwrap();
-        print!("{}: {}", size, line);
+        print!("{:4}: {}", size, line);
         if size == 0 {
             break;
         }
     }
-    print!("");
-
-    /* // accumurate whole output
-    let mut whole = Vec::new();
-    println!("start");
-    loop {
-        let mut buf: [u8; 7] = [0u8; 7];
-        let x = reader.read(&mut buf).unwrap();
-        println!("read: {}", x);
-        if x == 0 {
-            println!("end of stream");
-            break;
-        }
-        println!("copy");
-        whole.extend_from_slice(&buf);
-    }
-    println!("all: {}", String::from_utf8_lossy(&whole));
-    */
-    /*
-    for frame in iter {
-        match frame {
-            Ok(frame) => print!("frame:{}", str::from_utf8(frame.as_bytes()).unwrap()),
-            Err(err) => println!("frame:err:{:?}", err),
-        }
-    }
-    */
+    println!("");
 }
