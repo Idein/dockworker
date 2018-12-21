b665cc06480b1b6d8d0ba57f1fc9bf479c79cdb1
eldesh <nephits@gmail.com>
2018-12-21 04:56:40 -0800

    fix: make the event example to be completed by oneself

--- examples/events.rs
+++ examples/events.rs
@@ -1,14 +1,28 @@
 extern crate dockworker;
 
-use dockworker::Docker;
-use std::io::Write;
-use std::io::stdout;
+use dockworker::{ContainerCreateOptions, Docker};
 
 fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
     let events = docker.events(None, None, None).unwrap();
-    for event in events {
-        println!("{:?}", event);
-        stdout().flush();
-    }
+
+    let create = ContainerCreateOptions::new("hello-world:linux");
+    docker
+        .create_image("hello-world", "linux")
+        .unwrap()
+        .for_each(drop);
+    let container = docker.create_container(None, &create).unwrap();
+    docker.start_container(&container.id).unwrap();
+
+    events
+        .map(|e| e.unwrap())
+        .map(|e| {
+            println!("{:?}", e);
+            e
+        })
+        .find(|e| e.Type == "network" && e.Action == "disconnect");
+
+    docker
+        .remove_container(&container.id, None, Some(true), None)
+        .unwrap();
 }
