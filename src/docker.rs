57cdba3e9fef7db5faf8c29f9a5c1fa984de1022
Milad Rastian <eslashmili@gmail.com>
2019-01-22 13:03:24 +0100

    unify testing image with container_attach test

--- src/docker.rs
+++ src/docker.rs
@@ -1295,7 +1295,6 @@ mod tests {
         docker
             .remove_container(&container.id, None, None, None)
             .unwrap();
-        docker.remove_image(image_name, None, None).unwrap();
     }
 
     /// This is executed after `docker-compose build iostream-exec`
@@ -1307,7 +1306,7 @@ mod tests {
         // expected files
         let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("docker/attach");
         let exps: &[&str; 2] = &["./sample/apache-2.0.txt", "./sample/bsd4.txt"];
-        let image_name = "test-iostream-exec:latest";
+        let image_name = "test-iostream:latest";
 
         let host_config = ContainerHostConfig::new();
         let mut create = ContainerCreateOptions::new(image_name);
@@ -1358,7 +1357,6 @@ mod tests {
         docker
             .remove_container(&container.id, None, None, None)
             .unwrap();
-        docker.remove_image(image_name, None, None).unwrap();
     }
 
     /// This is executed after `docker-compose build signal`
