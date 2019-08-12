7c652bde4d202f5f4d673b18f0e48884f020a23d
Idanski <idanski1@gmail.com>
2019-08-12 19:01:27 +0300

    use &str instead of Container when only id is needed

--- examples/findports.rs
+++ examples/findports.rs
@@ -9,7 +9,7 @@ fn find_all_exported_ports() -> Result<()> {
     let docker = Docker::connect_with_defaults()?;
     let containers = docker.containers(ContainerListOptions::default())?;
     for container in &containers {
-        let info = docker.container_info(&container)?;
+        let info = docker.container_info(container.Id.as_str())?;
 
         // Uncomment this to dump everything we know about a container.
         //println!("{:#?}", &info);
