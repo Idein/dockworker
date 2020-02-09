a1e859afb498d3cd191c2814fbf47853d1f7fda9
eldesh <nephits@gmail.com>
2020-02-09 20:36:56 +0900

    tweak: network types

--- examples/findports.rs
+++ examples/findports.rs
@@ -7,16 +7,16 @@ use failure::Fail;
 
 fn find_all_exported_ports() -> Result<()> {
     let docker = Docker::connect_with_defaults()?;
-    let containers = docker.containers(ContainerListOptions::default())?;
+    let containers = docker.containers(ContainerListOptions::default().all())?;
     for container in &containers {
         let info = docker.container_info(container.Id.as_str())?;
 
         // Uncomment this to dump everything we know about a container.
         //println!("{:#?}", &info);
 
-        if let Some(ports) = info.NetworkSettings.Ports {
-            let ports: Vec<String> = ports.keys().cloned().collect();
-            println!("{}: {}", &info.Name, ports.join(", "));
+        println!("{}", info.Name);
+        for (k, v) in info.NetworkSettings.Ports.iter() {
+            println!("{}: {:?}", k, v);
         }
     }
     Ok(())
