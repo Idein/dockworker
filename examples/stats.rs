855dded62b521d69a20f98bae97364d7c284f808
takayuki goto <takayuki@idein.jp>
2020-12-21 19:54:19 +0900

    change: enumerates containers stat

--- examples/stats.rs
+++ examples/stats.rs
@@ -5,9 +5,12 @@ use dockworker::{ContainerListOptions, Docker};
 fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
     let opts = ContainerListOptions::default();
-    if let Some(container) = docker.containers(opts).unwrap().get(0) {
-        for stats in docker.stats(&container.Id, None, None).unwrap() {
-            println!("{:#?}", stats);
+    for container in docker.containers(opts).unwrap() {
+        for stats in docker
+            .stats(&container.Id, Some(false), Some(true))
+            .unwrap()
+        {
+            println!("{:#?}", stats.unwrap());
         }
     }
 }
