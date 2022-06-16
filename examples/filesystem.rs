c89ce6e8075c04a817364b81badf5871d68f6d00
dalance <dalance@gmail.com>
2022-06-16 09:27:28 +0900

    remove deprecated API in examples

--- examples/filesystem.rs
+++ examples/filesystem.rs
@@ -1,11 +1,14 @@
 extern crate dockworker;
 
-use dockworker::{ContainerListOptions, Docker};
+use dockworker::{container::ContainerFilters, Docker};
 
 fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
-    let opts = ContainerListOptions::default();
-    if let Some(container) = docker.containers(opts).unwrap().get(0) {
+    if let Some(container) = docker
+        .list_containers(None, None, None, ContainerFilters::default())
+        .unwrap()
+        .get(0)
+    {
         for change in docker.filesystem_changes(container.Id.as_str()).unwrap() {
             println!("{:#?}", change);
         }
