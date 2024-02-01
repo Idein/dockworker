aefd35ae9443b86fcad41245a2816ed49e71d019
tawashichan <ryotairie123@gmail.com>
2024-02-01 16:03:44 +0900

    chore: fmt

--- examples/port_mapping.rs
+++ examples/port_mapping.rs
@@ -5,8 +5,8 @@ async fn main() {
     let docker = Docker::connect_with_defaults().unwrap();
     let mut create = ContainerCreateOptions::new("nginx:latest");
     create.tty(true);
-    create.exposed_ports(ExporsedPorts(vec![(80,"tcp".to_string())]));
-    create.port_bindings(PortBindings(vec![(80,"tcp".to_string(),8080)]));
+    create.exposed_ports(ExporsedPorts(vec![(80, "tcp".to_string())]));
+    create.port_bindings(PortBindings(vec![(80, "tcp".to_string(), 8080)]));
     let container = docker
         .create_container(Some("testing"), &create)
         .await
