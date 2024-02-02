3d9de409750625b7483e98af743bc467667e7303
tawashichan <ryotairie123@gmail.com>
2024-02-02 14:03:21 +0900

    fix: fix example

--- examples/port_mapping.rs
+++ examples/port_mapping.rs
@@ -1,4 +1,6 @@
-use dockworker::{ContainerCreateOptions, Docker, ExporsedPorts, HostConfig, PortBindings};
+use dockworker::{
+    ContainerCreateOptions, ContainerHostConfig, Docker, ExporsedPorts, HostConfig, PortBindings,
+};
 
 #[tokio::main]
 async fn main() {
@@ -6,9 +8,9 @@ async fn main() {
     let mut create = ContainerCreateOptions::new("nginx:latest");
     create.tty(true);
     create.exposed_ports(ExporsedPorts(vec![(80, "tcp".to_string())]));
-    create.host_config(HostConfig {
-        port_bindings: PortBindings(vec![(80, "tcp".to_string(), 8080)]),
-    });
+    let mut host_config = ContainerHostConfig::new();
+    host_config.port_bindings(PortBindings(vec![(80, "tcp".to_string(), 8080)]));
+
     let container = docker
         .create_container(Some("test"), &create)
         .await
