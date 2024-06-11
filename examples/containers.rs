bcc5fa2f52c4b5ab92360148b13cf6049364cd8d
Luke Lynch <Tech.Luk3@gmail.com>
2024-06-11 21:01:12 +1000

    Updated containers example to cover task spawn

--- examples/containers.rs
+++ examples/containers.rs
@@ -1,15 +1,18 @@
 use dockworker::{container::ContainerFilters, Docker};
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
-    let docker = Docker::connect_with_defaults().unwrap();
-    let filter = ContainerFilters::new();
-    let containers = docker
-        .list_containers(None, None, None, filter)
-        .await
-        .unwrap();
+    task::spawn(async {
+        let docker = Docker::connect_with_defaults().unwrap();
+        let filter = ContainerFilters::new();
+        let containers = docker
+            .list_containers(None, None, None, filter)
+            .await
+            .unwrap();
 
-    containers.iter().for_each(|c| {
-        println!("{c:?}");
-    });
+        containers.iter().for_each(|c| {
+            println!("{c:?}");
+        });
+    });    
 }
