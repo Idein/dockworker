66cbdaa0d2fde52afce8ae4d7204701f0b29d4f9
Luke Lynch <Tech.Luk3@gmail.com>
2024-06-15 10:45:00 +1000

    Revert updating the containers example

--- examples/containers.rs
+++ examples/containers.rs
@@ -1,18 +1,15 @@
 use dockworker::{container::ContainerFilters, Docker};
-use tokio::task;
 
 #[tokio::main]
 async fn main() {
-    task::spawn(async {
-        let docker = Docker::connect_with_defaults().unwrap();
-        let filter = ContainerFilters::new();
-        let containers = docker
-            .list_containers(None, None, None, filter)
-            .await
-            .unwrap();
+    let docker = Docker::connect_with_defaults().unwrap();
+    let filter = ContainerFilters::new();
+    let containers = docker
+        .list_containers(None, None, None, filter)
+        .await
+        .unwrap();
 
-        containers.iter().for_each(|c| {
-            println!("{c:?}");
-        });
+    containers.iter().for_each(|c| {
+        println!("{c:?}");
     });
 }
