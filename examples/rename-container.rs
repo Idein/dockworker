53dd2f59077b877a9cd3cdd84c29788bf5f55594
takayuki goto <takayuki@idein.jp>
2025-03-27 15:57:55 +0900

    add: an example using rename container api

---
+++ examples/rename-container.rs
@@ -0,0 +1,16 @@
+use dockworker::{ContainerCreateOptions, Docker};
+
+#[tokio::main]
+async fn main() {
+    let docker = Docker::connect_with_defaults().unwrap();
+    let create = ContainerCreateOptions::new("hello-world:linux");
+    let container = docker
+        .create_container(Some("testing"), &create)
+        .await
+        .unwrap();
+    docker
+        .rename_container("testing", "testing-renamed")
+        .await
+        .unwrap();
+    println!("{container:?}")
+}
